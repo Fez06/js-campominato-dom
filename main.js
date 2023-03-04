275b6eca4cd87cdf04d529552b04e109d476d779
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-04 13:54:29 +0100

    quando schiaccio una bomba diventa rosso

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function facTotum (htmlElement, cssClass, htmlContainer, valoreNumerazione) {
 function eviStampa (target, cssClass, htmlValue) {
     target.addEventListener('click', function(){
     target.classList.add(cssClass);
-    console.log(htmlValue)
+    console.log(htmlValue);
  })
 }
 
@@ -126,16 +126,27 @@ function fillArrayUnicCasual(nValori, valoreMax, arrayName) {
     while (i < nValori) {
 
         const posValue = randomNumber(1, valoreMax);
-        console.log('roll');
-        console.log(posValue);
+        //console.log('roll');
+        //console.log(posValue);
         if (controllore(arrayName, posValue) === false) {
             arrayName.push(posValue);
             i++;
         }
 
     }
+    //console.log(arrayName);
 }
 
+    //questa mi serve perche' se no, avendo sia cell che cellbomb  l'elemento html mi mancherebbe nel momento dell'assegnazione della classe 
+function celleXDif (inputDifficulty, target) {
+    if (inputDifficulty === 'easy') {
+        assegnaClasse('cellEasy', target);
+
+    } else if (inputDifficulty === 'medium') {
+        assegnaClasse('cellMedium', target);
+
+    } else assegnaClasse('cellHard', target);
+}
 
 
 
@@ -150,7 +161,7 @@ const start = document.getElementById('start');
 const gruppoBombe = [];
 
 //controllore(gruppoBombe, '4');
-fillArrayUnicCasual(16, 20, gruppoBombe);
+//fillArrayUnicCasual(16, 20, gruppoBombe);
 
 
 //  Difficolta' //
@@ -162,32 +173,48 @@ const difficulty = document.getElementById('difSelect');
 start.addEventListener('click', function (){
 
     
-
+        //pulisco il board
     containerBoard.innerText = '';
+        //ottengo la difficolta' aka numero di celle totali
     const inputDifficulty = difficulty.value;
-
     let x = nCicli(inputDifficulty);
 
+        //creo le posizioni con le bombe ma per ora non sono esistenti in html
+    fillArrayUnicCasual(16, x, gruppoBombe);
 
+        
 
-
-    /*
+    
     for (let i = 1; i <= x; i++) {
 
-        let cella = facTotum('div','cell', containerBoard, i);
-        eviStampa(cella, 'highlight', i);
+           //se i corrisponde ad un valore di gruppobombe gli assegno classe diversa altimenti tutto normale
 
-        if (inputDifficulty === 'easy') {
+        if (controllore(gruppoBombe, i) === true) {
+            let cellaBomba = facTotum('div','cell', containerBoard, i);
+            eviStampa(cellaBomba, 'explode', i);
+            celleXDif(inputDifficulty, cellaBomba);
+
+        } else {
+            let cella = facTotum('div','cell', containerBoard, i);
+            eviStampa(cella, 'highlight', i);
+            celleXDif(inputDifficulty, cella);
+        }
+
+        /*if (inputDifficulty === 'easy') {
             assegnaClasse('cellEasy', cella);
 
         } else if (inputDifficulty === 'medium') {
             assegnaClasse('cellMedium', cella);
 
-        } else assegnaClasse('cellHard', cella);
+        } else assegnaClasse('cellHard', cella);*/
     
     }
-    */
+    
 })
+
+
+
+
 
 
 
